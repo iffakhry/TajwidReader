37f93f753edfd4f2b984dcb0758db7954af2c7de
Fakhry Ikhsan Firdaus <fakhry.ikhsan.firdaus@gmail.com>
2017-06-14 14:20:16 +0700

    Fixed bug gradle with opencv, create menu ImageDetection and RealtimeDetection,

--- app/src/main/java/net/crevion/fakhry/tajwidreader/MainActivity.java
+++ app/src/main/java/net/crevion/fakhry/tajwidreader/MainActivity.java
@@ -1,9 +1,12 @@
 package net.crevion.fakhry.tajwidreader;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -16,93 +19,114 @@ import org.opencv.core.Mat;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
-public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+public class MainActivity extends AppCompatActivity {
 
-    private static String TAG = "MainActivity";
-    JavaCameraView javaCameraView;
-    Mat mRgba, imgGray, imgCanny, imgBiner, imgMat;
-    // to fix camera orientation
-//    Mat mRgbaT, mRgbaF;
-    BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status) {
-                case BaseLoaderCallback.SUCCESS: {
-                    javaCameraView.enableView();
-                    break;
-                }
-                default: {
-                    super.onManagerConnected(status);
-                    break;
-                }
-            }
-        }
-    };
+//    private static String TAG = "MainActivity";
+//    JavaCameraView javaCameraView;
+//    Mat mRgba, imgGray, imgCanny, imgBiner, imgMat;
+//    // to fix camera orientation
+////    Mat mRgbaT, mRgbaF;
+//    BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
+//        @Override
+//        public void onManagerConnected(int status) {
+//            switch (status) {
+//                case BaseLoaderCallback.SUCCESS: {
+//                    javaCameraView.enableView();
+//                    break;
+//                }
+//                default: {
+//                    super.onManagerConnected(status);
+//                    break;
+//                }
+//            }
+//        }
+//    };
+//
+//    static {
+//
+//    }
 
-    static {
-
-    }
+    private Button imageButton, realtimeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        javaCameraView = (JavaCameraView)findViewById(R.id.java_camera_view);
-        javaCameraView.setVisibility(SurfaceView.VISIBLE);
-        javaCameraView.setCvCameraViewListener(this);
+        imageButton = (Button)findViewById(R.id.imagebutton);
+        realtimeButton = (Button)findViewById(R.id.realtimebutton);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent imageIntent = new Intent(MainActivity.this, ImageDetection.class);
+                startActivity(imageIntent);
+
+            }
+        });
+
+        realtimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent realtimeIntent = new Intent(MainActivity.this, RealtimeDetection.class);
+                startActivity(realtimeIntent);
+            }
+        });
+//        javaCameraView = (JavaCameraView)findViewById(R.id.java_camera_view);
+//        javaCameraView.setVisibility(SurfaceView.VISIBLE);
+//        javaCameraView.setCvCameraViewListener(this);
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (javaCameraView!=null)
-            javaCameraView.disableView();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (javaCameraView!=null)
-            javaCameraView.disableView();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (OpenCVLoader.initDebug()){
-            Log.i(TAG, "Open cv Loaded success");
-            mLoaderCallBack.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-        }else{
-            Log.i(TAG, "Open cv not loaded");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallBack);
-        }
-    }
-
-    @Override
-    public void onCameraViewStarted(int width, int height) {
-        mRgba = new Mat(height, width, CvType.CV_8UC4);
-//        mRgbaF = new Mat(height, width, CvType.CV_8UC4);
-//        mRgbaT = new Mat(height, width, CvType.CV_8UC4);
-        imgGray = new Mat(height, width, CvType.CV_8UC1);
-        imgCanny = new Mat(height, width, CvType.CV_8UC1);
-        imgMat= new Mat(height, width, CvType.CV_8UC1);
-    }
-
-    @Override
-    public void onCameraViewStopped() {
-        mRgba.release();
-    }
-
-    @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        mRgba = inputFrame.rgba();
-        //to rotate mRgba 90 degrees when portait
-//        Core.transpose(mRgba, mRgbaT);
-//        Imgproc.resize(mRgbaT, mRgbaF, mRgbaF.size(),0,0,0);
-//        Core.flip(mRgbaF, mRgba, 1);
-        Imgproc.cvtColor(mRgba, imgGray, Imgproc.COLOR_RGB2GRAY);
-        Imgproc.GaussianBlur(imgGray, imgGray, new Size(3,3),0);
-        Imgproc.threshold(imgGray, imgGray, 150, 255, Imgproc.THRESH_BINARY_INV);
-//        Imgproc.Canny(imgGray, imgCanny, 50, 150);
-        return imgGray;
-    }
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        if (javaCameraView!=null)
+//            javaCameraView.disableView();
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        if (javaCameraView!=null)
+//            javaCameraView.disableView();
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        if (OpenCVLoader.initDebug()){
+//            Log.i(TAG, "Open cv Loaded success");
+//            mLoaderCallBack.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+//        }else{
+//            Log.i(TAG, "Open cv not loaded");
+//            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallBack);
+//        }
+//    }
+//
+//    @Override
+//    public void onCameraViewStarted(int width, int height) {
+//        mRgba = new Mat(height, width, CvType.CV_8UC4);
+////        mRgbaF = new Mat(height, width, CvType.CV_8UC4);
+////        mRgbaT = new Mat(height, width, CvType.CV_8UC4);
+//        imgGray = new Mat(height, width, CvType.CV_8UC1);
+//        imgCanny = new Mat(height, width, CvType.CV_8UC1);
+//        imgMat= new Mat(height, width, CvType.CV_8UC1);
+//    }
+//
+//    @Override
+//    public void onCameraViewStopped() {
+//        mRgba.release();
+//    }
+//
+//    @Override
+//    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+//        mRgba = inputFrame.rgba();
+//        //to rotate mRgba 90 degrees when portait
+////        Core.transpose(mRgba, mRgbaT);
+////        Imgproc.resize(mRgbaT, mRgbaF, mRgbaF.size(),0,0,0);
+////        Core.flip(mRgbaF, mRgba, 1);
+//        Imgproc.cvtColor(mRgba, imgGray, Imgproc.COLOR_RGB2GRAY);
+//        Imgproc.GaussianBlur(imgGray, imgGray, new Size(3,3),0);
+//        Imgproc.threshold(imgGray, imgGray, 150, 255, Imgproc.THRESH_BINARY_INV);
+////        Imgproc.Canny(imgGray, imgCanny, 50, 150);
+//        return imgGray;
+//    }
 }
